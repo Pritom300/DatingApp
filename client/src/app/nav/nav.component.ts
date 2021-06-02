3084b40de4279029972d7325806d2070a54c0095
Pritom300 <pritomsarkar300@gmail.com>
2021-06-02 12:48:35 +0600

    End Of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -15,7 +17,8 @@ export class NavComponent implements OnInit {
   
  
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService : AccountService , private router: Router , 
+    private toastr:ToastrService) { }
 
   ngOnInit(): void {
     
@@ -25,11 +28,14 @@ export class NavComponent implements OnInit {
   {
     this.accountService.login(this.model).subscribe(response =>{
 
+      this.router.navigateByUrl('/members');
+
       console.log(response);
      
     },error=>{
 
       console.log(error);
+      this.toastr.error(error.error);
     }
     
     )
@@ -38,6 +44,7 @@ export class NavComponent implements OnInit {
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   
   }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input,Output, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class RegisterComponent implements OnInit {
 
   model:any={};
 
-  constructor(private accountService:AccountService) { }
+  constructor(private accountService:AccountService , private toastr : ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -25,6 +26,7 @@ export class RegisterComponent implements OnInit {
       this.cancel();
     },error=>{
       console.log(error);
+      this.toastr.error(error.error);
     })
    
   }

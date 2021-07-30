319e24708f9c6dc054f5cefefc858d3ccd6dc9a7
Pritom300 <pritomsarkar300@gmail.com>
2021-07-30 15:54:29 +0600

    End of section 9

--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -3,7 +3,7 @@ import { Photo } from './photo';
 
 export interface Member {
     id: number;
-    username: string;
+    userName: string;
     photoUrl: string;
     age: number;
     knownAs: string;
